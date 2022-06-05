9ef0e931bb7b4581cbbaa6d1067b51ae66127a8a
AmharMahir <89715117+AmharMahir@users.noreply.github.com>
2022-06-05 11:25:37 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+<?php include'header.php';?>
+<body style="background-color:#ffd8d4;">
+<style>
+.massage{
+  margin: 10px auto;
+  width: 800px;
+}
+</style>
+
+  <center><h3>Director Massage</h3>
+  <img src="images/3.jpg" alt="Director"width="600px" height="300px">  </center>
+<p class="massage">Our students are welcoming and accepting. Eager and interested in learning, they follow their passions and personalize their education. Upon graduation, they matriculate to universities of their choice all over the world. The IB Diploma, widely acknowledged as the best preparation for post-secondary success, is highly recognized worldwide and provides the widest reach to all university systems across all nations..</p>
+<hr>
+<center><h3>Principal Massage</h3>
+<img src="images/4.jpg" alt="Principal" width="600px" height="300px">  </center>
+<p class="massage">LOur students are exposed to a variety of values that are different from their own. In this environment, and through the delivery of our curriculum and approaches, we teach children how to be flexible and adaptable in life; that we are not competing, but rather we are winning when we cooperate and collaborate and embrace our differences.”.</p>
+
+<?php include'footer.php';?>
+</body>
+</html>
